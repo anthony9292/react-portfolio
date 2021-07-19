3e98e0794096180fc2e3521618962b2b6d9c8113
Anthony Ndegwa <anthon.n24@gmail.com>
2021-07-19 17:53:30 -0400

    got topbar to work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
+import Topbar from "./components/topbar/Topbar"
+
+
+
+
 
 function App( ) {
   return (
     <div className ="app">
-     top 
-     sec - into -portfolio  -work......
-     </div>
-
-  
+   <Topbar/>
+   </div>
     );
 
   }

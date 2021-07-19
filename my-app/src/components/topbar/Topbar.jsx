787f636d15060c70d4771d8fea768c86b7fe42c0
Anthony Ndegwa <anthon.n24@gmail.com>
2021-07-19 19:43:11 -0400

    still working on taskbar added icons

--- my-app/src/components/topbar/Topbar.jsx
+++ my-app/src/components/topbar/Topbar.jsx
@@ -1,9 +1,32 @@
-import  './topbar.scss'
+import  './topbar.scss';
+import {PhoneAndroid,Email} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left"> 
+                        <a href="#intros" className="logo"> NDEGWA.</a> 
+                        <div className="itemContainer">
+                            <PhoneAndroid className="icon"/>
+                            <span>613-501-4892</span>
+                        </div>
+                        <div className="itemContainer"> 
+                         <Email className="icon"/> 
+                         <span>anthon.n24@gmail.com</span>
+                        
+                        </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"> </span>
+                        <span className="line2"> </span>
+                        <span className="line3"> </span>
+                    </div>
+                
+                        
+                </div>
+            </div>
         </div>
     )
 }

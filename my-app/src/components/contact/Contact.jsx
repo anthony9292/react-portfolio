e3d623d6ebd123f01711d5c8dfe07e44c262469a
Anthony Ndegwa <anthon.n24@gmail.com>
2021-07-20 00:55:54 -0400

    taskbar done

--- my-app/src/components/contact/Contact.jsx
+++ my-app/src/components/contact/Contact.jsx
@@ -2,7 +2,7 @@ import "./contact.scss";
 
 export default function Contact() { 
     return ( 
-        <div className="contact" id="contacts">
+        <div className="contact" id="contact">
             
         </div>
     )
--- my-app/src/components/menu/Menu.jsx
+++ my-app/src/components/menu/Menu.jsx
@@ -1,23 +1,24 @@
 import "./menu.scss";
 
-export  default function Menu()  {
+export  default function Menu({ menuOpen, setMenuOpen })  {
     return (
         /*hyperlink for sections in topbar*/
-        <div className="menu">
+        <div className={"menu " + (menuOpen && "active")}>
             <ul>
-                <li>
+                {/*Onclick action to close menu tap when clicking on one  of hyperlink(home,port..ex)*/}
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#intro">Home</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#portfolio">portfolio</a>
                 </li>
-                <li>
-                    <a href="#works">Works</a>
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#work">Works</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#testimonials">Testimonials</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#contact">Contact Info</a>
                 </li> 
             </ul>

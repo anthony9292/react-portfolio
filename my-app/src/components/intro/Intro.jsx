58de71810f8aae667c2904ab4aaed4003ed0e626
Anthony Ndegwa <anthon.n24@gmail.com>
2021-07-20 15:23:31 -0400

    intro,nice arrow blinking animations done

--- my-app/src/components/intro/Intro.jsx
+++ my-app/src/components/intro/Intro.jsx
@@ -8,16 +8,16 @@ export default function intro() {
                   <img src="assets/cover.png" alt=""/>
                   </div>
               </div>
-            <div className="right">
-                <div className="wrapper">
-                    <h2>Hello, Welcome</h2>
-                    <h1> To my Personal </h1>
-                    <h3>Portfolio<span></span></h3>
-                </div>
-                <a href="#portfolio">
-                  
-                </a>
-            </div>
+             <div className="right">
+        <div className="wrapper">
+          <h2>Hello, and welcome</h2>
+          <h1>to my Web</h1>
+          <h3> Portfolio<span></span></h3>
         </div>
-    )
+        <a href="#portfolio">
+          <img src="assets/downarrow.png" alt=""/>
+        </a>
+      </div>
+    </div>
+    );
 }
